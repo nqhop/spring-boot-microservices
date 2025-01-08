7d9fde48601951e12785ba12688526d09c474b16
Nguyễn Quốc Hợp <hop35889@gmail.com>
2025-01-08 22:57:16 +0700

    spring-boot-postgres

---
+++ spring-boot-postgres/src/main/java/com/demo/pg/spring_boot_postgres/SpringBootPostgresApplication.java
@@ -0,0 +1,13 @@
+package com.demo.pg.spring_boot_postgres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPostgresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPostgresApplication.class, args);
+	}
+
+}
---
+++ spring-boot-postgres/src/test/java/com/demo/pg/spring_boot_postgres/SpringBootPostgresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.pg.spring_boot_postgres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPostgresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
